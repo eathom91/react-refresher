484632f1b3e650007d19ed0e2416dfeae6704f9b
eric <eathom91@gmail.com>
2022-08-21 17:54:05 -0400

    Updated components

--- README.md
+++ README.md
@@ -9,3 +9,7 @@
 5) HTML Tags used in component functions are still react components even though they look like plain html tags.
 
 6) When pointing to custom functions in something like an onClick attribute for a button, you do not want to call the method directly such as deleteHandler() because it will execute immediately when the Todo is rendered later in App, and we only want it to render when clicked.
+
+7) When using useState, setting the value with the second array index function will cause React to re-execute the component function that it's bound to.
+
+8)
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,20 +1,27 @@
-import { useState } from "react"; 'react'
+import { useState } from "react";
+import Backdrop from "./Backdrop";
+import Modal from "./Modal";
 function Todo(props) {
   const [modalIsOpen, setModelIsOpen] = useState(false);
 
-  function deleteHandler() {
-   
+  function openModalHandler() {
+    setModelIsOpen(true);
+  }
+
+  function closeModalHandler() {
+    setModelIsOpen(false);
   }
 
   return (
-    <div>
-      <div className="card">
-        <h2>{props.text}</h2>
-        <div className="actions">
-          <button className="btn"
-          onClick={deleteHandler}>Delete</button>
-        </div>
+    <div className="card">
+      <h2>{props.text}</h2>
+      <div className="actions">
+        <button className="btn" onClick={openModalHandler}>
+          Delete
+        </button>
       </div>
+      {modalIsOpen && <Modal onConfirm={closeModalHandler} onCancel={closeModalHandler} />}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
     </div>
   );
 }
